b1a18b413d685a49a16b5f3bae3d8a1ed21aa847
Micah Stubbs <micah.stubbs@gmail.com>
2019-04-11 18:26:02 -0700

    add example with small dataset and format flare.json

---
+++ iterations/02/README.md
@@ -0,0 +1,9 @@
+an iteration that draws the a smaller dataset.
+
+If your tree dataset is small, then this example should be useful for showing how your dataset might look as a dendrogram.
+
+---
+
+D3’s [cluster layout](https://github.com/d3/d3-hierarchy/blob/master/README.md#cluster) produces node-link diagrams with leaf nodes at equal depth. These are less compact than [tidy trees](/@d3/tidy-tree), but are useful for dendrograms, hierarchical clustering and [phylogenetic trees](/@mbostock/tree-of-life). See also the [radial variant](/@d3/radial-dendrogram).`
+
+inspired by https://observablehq.com/@d3/cluster-dendrogram
---
+++ iterations/02/index.js
@@ -0,0 +1,94 @@
+// define some constants
+const width = 960
+const height = 2500
+const margin = { top: 0, left: 0, bottom: 0, right: 50 }
+const innerWidth = width - margin.left - margin.right
+const innerHeight = height - margin.top - margin.bottom
+
+// load the data
+d3.json('analytics.json').then(data => draw(data))
+
+function draw(data) {
+  // define the tree layout function
+  const tree = data => {
+    const root = d3
+      .hierarchy(data)
+      .sort(
+        (a, b) => a.height - b.height || a.data.name.localeCompare(b.data.name)
+      )
+    root.dx = 10
+    root.dy = innerWidth / (root.height + 1)
+    return d3.cluster().nodeSize([root.dx, root.dy])(root)
+  }
+
+  // call the tree layout function on the data
+  const root = tree(data)
+
+  // draw the visualization
+  let x0 = Infinity
+  let x1 = -x0
+  root.each(d => {
+    if (d.x > x1) x1 = d.x
+    if (d.x < x0) x0 = d.x
+  })
+
+  const svg = d3
+    .select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(40,0)')
+
+  const g = svg
+    .append('g')
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', 10)
+    .attr('transform', `translate(${root.dy / 3},${root.dx - x0})`)
+
+  const link = g
+    .append('g')
+    .attr('fill', 'none')
+    .attr('stroke', '#555')
+    .attr('stroke-opacity', 0.4)
+    .attr('stroke-width', 1.5)
+    .selectAll('path')
+    .data(root.links())
+    .join('path')
+    .attr(
+      'd',
+      d => `
+        M${d.target.y},${d.target.x}
+        C${d.source.y + root.dy / 2},${d.target.x}
+         ${d.source.y + root.dy / 2},${d.source.x}
+         ${d.source.y},${d.source.x}
+      `
+    )
+
+  const node = g
+    .append('g')
+    .attr('stroke-linejoin', 'round')
+    .attr('stroke-width', 3)
+    .selectAll('g')
+    .data(root.descendants().reverse())
+    .join('g')
+    .attr('transform', d => `translate(${d.y},${d.x})`)
+
+  node
+    .append('circle')
+    .attr('fill', d => (d.children ? '#555' : '#999'))
+    .attr('r', 2.5)
+
+  node
+    .append('text')
+    .attr('dy', '0.31em')
+    .attr('x', d => (d.children ? -6 : 6))
+    .text(d => d.data.name)
+    .filter(d => d.children)
+    .attr('text-anchor', 'end')
+    .clone(true)
+    .lower()
+    .attr('stroke', 'white')
+
+  // return svg.node()
+}
