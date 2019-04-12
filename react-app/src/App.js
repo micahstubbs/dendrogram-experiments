02ccd823011d8fbea20b073e83e6cb5b7e70a4d5
Micah Stubbs <micah.stubbs@gmail.com>
2019-04-11 21:00:02 -0700

    render Dendrogram inside example react-app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dendrogram from './components/Dendrogram/'
+import data from './data/analytics.json'
 
 class App extends Component {
   render() {
+    const width = 960
+    const height = 500
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <svg width={width} height={height} id="svg">
+          <Dendrogram x={0} y={0} width={width} height={height} data={data} />
+        </svg>
       </div>
     );
   }
